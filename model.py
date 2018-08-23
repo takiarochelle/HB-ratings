b6f379975ea5c94170db0e28aa59f67ab5642cee
Hackbright Student <no-reply@hackbrightacademy.com>
2018-08-22 21:03:05 -0700

    added solution, and added user and movie relationship to ratings

--- model.py
+++ model.py
@@ -2,14 +2,15 @@
 
 from flask_sqlalchemy import SQLAlchemy
 
-# This is the connection to the PostgreSQL database; we're getting this through
-# the Flask-SQLAlchemy helper library. On this, we can find the `session`
-# object, where we do most of our interactions (like committing, etc.)
+# This is the connection to the PostgreSQL database; we're getting
+# this through the Flask-SQLAlchemy helper library. On this, we can
+# find the `session` object, where we do most of our interactions
+# (like committing, etc.)
 
 db = SQLAlchemy()
 
 
-##############################################################################
+#####################################################################
 # Model definitions
 
 class User(db.Model):
@@ -17,49 +18,66 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
     email = db.Column(db.String(64), nullable=True)
     password = db.Column(db.String(64), nullable=True)
     age = db.Column(db.Integer, nullable=True)
     zipcode = db.Column(db.String(15), nullable=True)
 
     def __repr__(self):
-        """make the User Id more sensical"""
+        """Provide helpful representation when printed."""
 
         return f"<User user_id={self.user_id} email={self.email}>"
 
 
-# Put your Movie and Rating model classes here.
-
 class Movie(db.Model):
-    """Movies info on website"""
+    """Movie on ratings website."""
 
     __tablename__ = "movies"
 
-    movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    released_at = db.Column(db.DateTime, nullable=False)
-    imdb_url = db.Column(db.String(200), nullable=False)
+    movie_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    title = db.Column(db.String(100))
+    released_at = db.Column(db.DateTime)
+    imdb_url = db.Column(db.String(200))
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"<Movie movie_id={self.movie_id} title={self.title}>"
 
 
 class Rating(db.Model):
+    """Rating of a movie by a user."""
 
     __tablename__ = "ratings"
 
-    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    movie_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    score = db.Column(db.Integer, nullable=False)
+    rating_id = db.Column(db.Integer,
+                          autoincrement=True,
+                          primary_key=True)
+    movie_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return f"""<Rating rating_id={self.rating_id} 
+                   movie_id={self.movie_id} 
+                   user_id={self.user_id} 
+                   score={self.score}>"""
 
 
-
-##############################################################################
+#####################################################################
 # Helper functions
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
-    # Configure to use our PstgreSQL database
+    # Configure to use our PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
@@ -67,8 +85,9 @@ def connect_to_db(app):
 
 
 if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
-    # you in a state of being able to work with the database directly.
+    # As a convenience, if we run this module interactively, it will
+    # leave you in a state of being able to work with the database
+    # directly.
 
     from server import app
     connect_to_db(app)
--- seed.py
+++ seed.py
@@ -1,28 +1,18 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
+import datetime
 from sqlalchemy import func
-from model import User
-from datetime import datetime
-from model import Rating
-from model import Movie
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 from server import app
 
 
-
-def load_users():
+def load_users(user_filename):
     """Load users from u.user into database."""
 
     print("Users")
 
-    # Delete all rows in table, so if we need to run this a second time,
-    # we won't be trying to add duplicate users
-    # user id | age | gender | occupation | zip code
-    User.query.delete()
-
-    # Read u.user file and insert data
-    for row in open("seed_data/u.user"):
+    for i, row in enumerate(open(user_filename)):
         row = row.rstrip()
         user_id, age, gender, occupation, zipcode = row.split("|")
 
@@ -32,53 +22,89 @@ def load_users():
         # We need to add to the session or it won't ever be stored
         db.session.add(user)
 
+        # provide some sense of progress
+        if i % 100 == 0:
+            print(i)
+
     # Once we're done, we should commit our work
     db.session.commit()
 
 
-def load_movies():
+def load_movies(movie_filename):
     """Load movies from u.item into database."""
 
-    #Read the u.items
-    for row in open("seed_data/u.item"):
+    print("Movies")
+
+    for i, row in enumerate(open(movie_filename)):
         row = row.rstrip()
+
+        # clever -- we can unpack part of the row!
         movie_id, title, released_str, junk, imdb_url = row.split("|")[:5]
 
+        # The date is in the file as daynum-month_abbreviation-year;
+        # we need to convert it to an actual datetime object.
+
         if released_str:
-            released_at = datetime.strptime(released_str, '%d-%b-%Y')
+            released_at = datetime.datetime.strptime(released_str, "%d-%b-%Y")
         else:
             released_at = None
 
-        title = title[:-7]
+        # Remove the (YEAR) from the end of the title.
+
+        title = title[:-7]   # " (YEAR)" == 7
 
         movie = Movie(title=title,
                       released_at=released_at,
                       imdb_url=imdb_url)
 
+        # We need to add to the session or it won't ever be stored
         db.session.add(movie)
 
+        # provide some sense of progress
+        if i % 100 == 0:
+            print(i)
+
+    # Once we're done, we should commit our work
     db.session.commit()
 
 
-def load_ratings():
-    """Load ratings from u.data into database.
-    user_id | movie_id | score | timestamp."""
+def load_ratings(rating_filename):
+    """Load ratings from u.data into database."""
 
-    for row in open("seed_data/u.data"):
+    print("Ratings")
+
+    for i, row in enumerate(open(rating_filename)):
         row = row.rstrip()
-        user_id, movie_id, score, timestamp = row.split('\t')
+
+        user_id, movie_id, score, timestamp = row.split("\t")
+
         user_id = int(user_id)
         movie_id = int(movie_id)
         score = int(score)
+
+        # We don't care about the timestamp, so we'll ignore this
 
         rating = Rating(user_id=user_id,
                         movie_id=movie_id,
                         score=score)
 
+        # We need to add to the session or it won't ever be stored
         db.session.add(rating)
 
-    db.session.commit()
+        # provide some sense of progress
+        if i % 1000 == 0:
+            print(i)
 
+            # An optimization: if we commit after every add, the database
+            # will do a lot of work committing each record. However, if we
+            # wait until the end, on computers with smaller amounts of
+            # memory, it might thrash around. By committing every 1,000th
+            # add, we'll strike a good balance.
+
+            db.session.commit()
+
+    # Once we're done, we should commit our work
+    db.session.commit()
 
 
 def set_val_user_id():
@@ -96,12 +122,12 @@ def set_val_user_id():
 
 if __name__ == "__main__":
     connect_to_db(app)
-
-    # In case tables haven't been created, create them
     db.create_all()
 
-    # Import different types of data
-    load_users()
-    load_movies()
-    load_ratings()
+    user_filename = "seed_data/u.user"
+    movie_filename = "seed_data/u.item"
+    rating_filename = "seed_data/u.data"
+    load_users(user_filename)
+    load_movies(movie_filename)
+    load_ratings(rating_filename)
     set_val_user_id()
